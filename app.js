56068922aa5d504256794dbb8568ec632b8609e6
Sergio Vicente de las Heras <sergio.v.he@gmail.com>
2018-03-11 00:54:34 +0100

    Fix favicon error and night mode

--- app.js
+++ app.js
@@ -12,12 +12,9 @@ var api = require('./routes/api');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -36,7 +33,6 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV == 'development' ? err : {};
 
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
   var date = new Date()
+
+  // Madrid latitude: 40.416775, longitude: -3.703790
   var times = suncalc.getTimes(date, 40.41, -3.70);
 
-  if (times.sunrise.getHours() < date.getHours() <= times.sunset.getHours())
+  if (times.sunrise.getHours() < date.getHours() && date.getHours() <= times.sunset.getHours())
     var mode = 'day'
   else
     var mode = 'night'
